c4f04b00650620afbcec433b83172ff0f8d06f8b
szibert1993 <sereganesrock@gmail.com>
2017-10-04 20:29:50 +0600

    решена задача № 4343 (#1)

--- src/Lab01.java
+++ src/Lab01.java
@@ -12,7 +12,7 @@ public class Lab01 {
     }
 
     public static void task4343(String a, String b, String c, String d, String e) {
-        System.out.println("User ID=" + a + ";" + "Password=" + b + ";" + c + "Host=" + d + ";" + "Database=" + e + ";");
+        System.out.println("User ID=" + a + "; Password=" + b + "; " + c + "Host=" + d + "; Database=" + e + ";");
 
     }
 
