9a0b1c9e0b9df4e6a5d5ccc3894811b52a379843
szibert1993 <sereganesrock@gmail.com>
2017-10-06 20:40:49 +0600

    решена задача 9180

--- src/Lab06.java
+++ src/Lab06.java
@@ -12,9 +12,8 @@ public class Lab06 {
             A = A + 1;
         }
         return (sum);
- * Created by pro-27 on 06.10.2017.
- */
-public class Lab06 {
+    }
+
     public static double task2802(int n) throws Exception {
         if(n <= 0) {
             throw new Exception("Значение N должно быть положительным");
