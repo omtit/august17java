b8977101569cd3d9329f53161a9d5e8ebf2de725
abelyaev90 <tf37a31612@gmail.com>
2017-10-06 20:33:06 +0700

    Делаю задачу 3942, #42

--- src/Lab02.java
+++ src/Lab02.java
@@ -2,11 +2,23 @@
  * Created by Александр on 27.09.2017.
  */
 public class Lab02 {
-    public static double task4411(double x){
-        return 2*x;
+    public static double task4411(double x) {
+        return 2 * x;
     }
 
     public static double task4366(int x1, int x2) {
         return Math.sqrt(Math.pow(x2, 2) + Math.pow(x1, 2));
     }
-}
+
+
+    public static double task3943(double x, double y) throws Exception {
+        if (y < 0) {
+            throw new Exception("Значение y должно быть неотрицательным");
+        }
+        if ((x+Math.sqrt(y)) < 0) {
+            throw new Exception("Подкоренное выражение должно быть неотрицательным");
+        }
+            return -5 * Math.sqrt((x + Math.sqrt(y)));
+        }
+
+    }
